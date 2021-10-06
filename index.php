c187d5d3438b8c338673cd9e02b4dc77b7bdc6d8
EloiJuradoCabero <eloi.jurado@cirvianum.cat>
2021-10-06 16:28:45 +0200

    first commit

--- index.php
+++ index.php
@@ -1,1 +1,15 @@
 <?php
+session_start();
+
+
+$posts = array(
+    "username" => array("hector","pepe"),
+    "subjects" => array(),
+    "body" => array()
+
+);
+
+$_SESSION("llistaPosts",$posts);
+
+var_dump($_SESSION("llistaPosts"));
+?>
